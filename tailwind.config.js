fabcc76e8e4d64f09e9c5b2731e1c9af5f3d5e38
Trisha Huynh <78327418+trishahuynh7@users.noreply.github.com>
2025-02-02 15:23:15 -0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JavaScript and TypeScript files in the src directory and its subdirectories
+    "./App.js", // Include the App.js file in the root directory
+    "./index.js" // Include the index.js file in the root directory
+  ],
+  theme: {
+    extend: {}, // Add custom styles here
+  },
+  plugins: [], // Add any plugins here
+}
