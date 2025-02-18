4bb6b14e742672d763ba73b88de0d53a7c362312
Trisha Huynh <78327418+trishahuynh7@users.noreply.github.com>
2025-02-17 16:43:23 -0800

    Update Home.js
    added search bar for homepage

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Searchbar } from 'react-native-paper';
 
 export default function Home() {
   return (
     <View style={styles.container}>
+      // search bar
+      <View style={styles.search}>
+        <Searchbar
+          placeholder="Search YummyFoods Recipe"
+        />
+      </View>
+
+      // homepage text
       <Text style={styles.text}>This is the Home Screen</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -19,4 +28,13 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
+  search: {
+    height: 25,
+    borderRadius: 10,
+    padding: 10,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+  }
 });
