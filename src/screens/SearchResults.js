455ee7458189b2aada1fb89560c1f9c6869b1475
Trisha Huynh <78327418+trishahuynh7@users.noreply.github.com>
2025-02-26 11:18:50 -0800

    Add files via upload
    Added SearchResults as a screen to show recipe results from search bar

---
+++ src/screens/SearchResults.js
@@ -0,0 +1,15 @@
+// this page is where the recipes will show
+// as a result of inputting certain keywords 
+// in the search bar
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Searchbar } from 'react-native-paper';
+
+// recipe results page view & functionality
+export default function searchResults(){
+    const [searchInput, setSearchInput] = React.useState('');
+    
+};
+
+
+// recipe results page styling (font, format, etc.)
